d0ffe246849f5f126c122e52f9468de6518d4eb8
bluexm <Xavier.mathieu.office@gmail.com>
2017-12-01 14:06:19 +0800

    test with mkt data

---
+++ job_scrapper_IndeedHK.py
@@ -0,0 +1,168 @@
+import bs4
+import certifi
+import urllib3
+import re
+import requests as rqs
+import csv
+import datetime 
+import pandas as pd 
+import Levenshtein as levs 
+import scraperwiki as ws 
+
+## user params 
+url ='https://www.indeed.hk/jobs?q=Data+Scientist&start='
+nbpagesmax = 10
+
+def dict_value(tuple):
+	return tuple[-1]
+
+exectime = datetime.datetime.now()
+	
+
+ht = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
+
+csvfile = open('scrape_indeed_'+ exectime.strftime("%Y%m%d")+'.csv', 'w', newline='',encoding="utf-8")
+writer = csv.writer(csvfile, delimiter=';', quotechar="'", quoting=csv.QUOTE_MINIMAL)
+
+excelDBfilename = 'scraping_indeed.xlsx'
+df = pd.read_excel(excelDBfilename, 'Sheet1', index_col=None, na_values=['NA'])
+
+""" single ad page scrapers """
+def parse_workinginhongkong(pdata):
+	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
+	if adtree.find("div", class_="section_header") ==None: 
+		return 'NA','NA','NA','NA','NA'
+	else: 
+		addate = ''.join([s for s in adtree.find("div", class_="section_header").find("div", class_="date").stripped_strings])
+		adtitle = ''.join([s for s in adtree.find("div", class_="section_header").find("h1", class_="title").stripped_strings])
+		adcompany = adtree.find("div", class_="section_header").p.a.string
+
+		a = [s.stripped_strings for s in adtree.find("div", class_="section_content").find_all("p")]
+		adjobdes = ''.join([''.join(s) for s in a])
+		ademail = re.findall("[.\w]*@+\w+.com",pdata)
+	return addate, adtitle, adjobdes, adcompany, ademail
+
+def parse_indeed(pdata):
+	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
+	root= adtree.find("table", id="job-content")
+	#pdb.set_trace()
+	if root==None: 
+		return 'NA','NA','NA','NA','NA'
+	else: 
+		addate = str(''.join([s for s in root.find("span", class_="date").stripped_strings]))
+		adtitle = ''.join([s for s in root.find("b", class_="jobtitle").stripped_strings])
+		adcompany = root.find("span", class_="company").string
+		adjobdes = ''.join([s for s in root.find("span", id="job_summary").stripped_strings])
+		ademail = re.findall("[.\w]*@+\w+.com",pdata)
+		if ademail ==[]: ademail='NA'
+	return addate, adtitle, adjobdes, adcompany, ademail
+	
+def parse_gobee(pdata):
+	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
+	adtitle = adtree.find("div", class_="job-header").find("h1").string
+	
+	if adtitle==None: 
+		return 'NA','NA','NA','NA','NA'
+	else: 
+		st = tree.find("script", type="application/ld+json").string.title()
+		st = re.findall("Dateposted.*,",st)[0]
+		p = st.find(",")
+		addate = st[p-11:-2]
+		adcompany = "gobeebike"
+		adjobdes = ' '.join([s for s in adtree.find("div", class_="description").stripped_strings])
+		ademail = "NA"
+
+	return addate, adtitle, adjobdes, adcompany, ademail
+	
+def parse_classywheeler(pdata):
+	return '','','','',''
+
+def parse_efinancialcareers(pdata):
+	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
+	root = adtree.find("h1", itemprop="title")
+	#pdb.set_trace()
+	if root==None: 
+		return 'NA','NA','NA','NA','NA'
+	else: 
+		addate = ''.join([s for s in adtree.find("span", itemprop="datePosted").stripped_strings])
+		adtitle = ''.join([s for s in adtree.find("h1", itemprop="title").stripped_strings])	
+		adcompany = ''.join([s for s in adtree.find("span", itemprop="hiringOrganization").stripped_strings])
+		adjobdes = ''.join([s for s in adtree.find("div", itemprop="description").stripped_strings])
+		ademail = re.findall("[.\w]+@+\w+.com",pdata)
+		if ademail ==[]: ademail='NA'
+	return addate, adtitle, adjobdes, adcompany, ademail
+
+""" scraping from indeed.hk """ 
+res=[]
+for i in range(nbpagesmax):
+	try:
+		r = ht.request('GET',url+ str(i+1)+'0')
+		tx = r.data.decode("utf-8","ignore")
+
+		##scraping 
+		tree = bs4.BeautifulSoup(tx, 'html.parser')
+		#print(tree.prettify())
+		content = tree.find_all("div",class_=re.compile("row"))
+		
+		## ad sites with parsers 
+		adparsers = {	"workinginhongkong": parse_workinginhongkong, 
+						"indeed":parse_indeed,
+						"gobee":parse_gobee,
+						"efinancialcareers":parse_efinancialcareers
+						#"classywheeler": parse_classywheeler, 
+						#"efinancialcareers": parse_efinancialcareers
+					}
+		## iterates on all search results 
+		for c in content:
+			rowres=[exectime.timestamp(), exectime.strftime("%Y-%m-%d %H:%M:%S")]
+			#rowres.append(c.find_all("span",class_="company")[0].get_text())
+			#records company's listed on search
+			#pdb.set_trace()
+			rowres.append(''.join([s for s in c.find_all("span",class_="company")[0].stripped_strings]))
+			rowres.append(c.find_all("a",class_="turnstileLink")[0].get_text())
+			adlink = 'https://www.indeed.hk'+ c.find_all("a",class_="turnstileLink")[0]['href']
+			rowres.append(adlink)
+			
+			## get ad detailed infos 
+			ad = rqs.get(adlink)
+			adshorturl = ad.url
+			rowres.append(adshorturl)
+
+			##branch on correct parser
+			addate, adtitle, adcompany, adjobdes,  ademail = 'NotParsed','NotParsed','NotParsed','NotParsed','NotParsed'
+			for k in adparsers.keys():
+				if re.findall(k,adshorturl):
+					addate, adtitle, adcompany, adjobdes,  ademail = adparsers[k](ad.text)
+					break
+				
+			rowres += [addate, adtitle, adjobdes, adcompany, ademail]
+
+			## check if ad is already here or not
+			dorecord=True
+			print('\n', len(adlink), adlink)
+			for k in df['search_ad_url']:
+				ldist = levs.distance(adlink,k)
+				ldistratio = (len(k)-ldist)/len(adlink)
+				#print(ldist, ldistratio,end = ' ')
+				if ldistratio>0.95:
+					dorecord=False
+					break
+
+			## store and display results 
+			#print(' ; '.join([str(s) for s in rowres]))
+			writer.writerow(rowres)
+			#res.append(rowres)
+			if dorecord:	
+				# in dataframe for excel 
+				df = df.append( pd.DataFrame([rowres],columns=df.columns))
+				# in SQLlite 
+				titles = ["epoch","scrping_dt","ad_cie_indeed","ad_jobtitle_indeed","search_ad_url","ad_url","ad_jobdate","ad_jobtitle","ad_jobcie","ad_jobdes","ad_email"] 
+				ws.sqlite.save(unique_keys=['ad_url'], table_name="indeed_ads", data={x:y for (x,y) in zip(titles,rowres)} )
+	except: 
+		pass
+
+
+			
+print("end of scraping ---------------------------------------------------------")
+df.to_excel(excelDBfilename, sheet_name='Sheet1', index=False)
+csvfile.close()
--- scraper.py
+++ scraper.py
@@ -1,24 +1,178 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# coding=utf-8
+import os 
+import ccxt
+import pandas as pd 
+import time, datetime
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import sqlite3
+from sqlite3 import Error
+DB_FILE = '.\\orderbooks.db'
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+""" create a database connection to a SQLite database """
+try:
+	conn = sqlite3.connect(DB_FILE)
+	print(sqlite3.version)
+except Error as e:
+	print(e)
+
+#bitmex = ccxt.bitmex()
+
+## ANX PRO SETTINGS 
+anx = ccxt.anxpro()
+##Private key 
+api_key = os.environ.get('ANX_API_KEY') 
+api_secret = os.environ.get('ANX_SECRET')
+exmo = ccxt.exmo({
+    'apiKey': api_key,
+    'secret': api_secret
+})
+
+##Poloniex settings
+polo = ccxt.poloniex()
+##Private key 
+api_key = os.environ.get('POLONIEX_API_KEY') 
+api_secret = os.environ.get('POLONIEX_SECRET') 
+exmo_polo   = ccxt.exmo({
+    'apiKey': api_key,
+    'secret': api_secret
+})
+
+##BitMex settings
+bmex = ccxt.bitmex()
+##Private key 
+api_key = os.environ.get('BITMEX_API_KEY') 
+api_secret = os.environ.get('BITMEX_SECRET') 
+exmo_bmex   = ccxt.exmo({
+    'apiKey': api_key,
+    'secret': api_secret
+})
+
+##Bitfinex  settings
+bfix = ccxt.bitfinex()
+##Private key 
+api_key = os.environ.get('BITFINEX_API_KEY') 
+api_secret = os.environ.get('BITFINEX_SECRET') 
+exmo_bfix   = ccxt.exmo({
+    'apiKey': api_key,
+    'secret': api_secret
+})
+
+##Gatecoin  settings
+bgtcn = ccxt.gatecoin()
+##Private key 
+exmo_bgtcn   = ccxt.exmo({
+    'apiKey': '',
+    'secret': ''
+})
+
+
+
+def get_orderbook(ccxtobj,symbol,sourcename,connexion):
+	dictbook = ccxtobj.fetch_order_book(symbol)
+	print(dictbook['asks'][0])
+	a = pd.DataFrame(dictbook['asks'],columns=['askprice','askqty'])
+	a['rank']=a.index.values
+	b = pd.DataFrame(dictbook['bids'],columns=['bidprice','bidqty'])
+	b['rank']=b.index.values
+	df = pd.merge(a,b,on='rank')
+	df['source']=sourcename
+	df.index = [str(dictbook['timestamp']) for  r in df['rank']] #+"_"+str(r)
+	# record in DB
+	df.to_sql('orderbooks',connexion,if_exists='append')
+
+i=0
+while (i<5):
+	get_orderbook(bgtcn, "BTC/USD", 'gatecoin', conn)
+	#get_orderbook(anx, anx.symbols[0], 'anx', conn)
+	#get_orderbook(polo, "BTC/USDT", 'polo', conn)
+	#get_orderbook(bmex, "BTC/USD", 'bitmex', conn)
+	#get_orderbook(bfix, "BTC/USD", 'bitfinex', conn)
+	
+	time.sleep(2)
+	i+=1
+	
+	"""
+	## ANX
+	#anx_markets = anx.load_markets()
+	#print(anx.id, anx_markets)
+	#print(anx.fetch_order_book(anx.symbols[0]))	
+	
+	dictbook = anx.fetch_order_book(anx.symbols[0])
+	pd.DataFrame(dictbook['bids'],columns=['price','qty'])
+	a = pd.DataFrame(dictbook['asks'],columns=['askprice','askqty'])
+	a['rank']=a.index.values
+	b = pd.DataFrame(dictbook['bids'],columns=['bidprice','bidqty'])
+	b['rank']=b.index.values
+	df_anx = pd.merge(a,b,on='rank')
+	df_anx['source']='anx'
+	df_anx.index = [str(dictbook['timestamp']) for  r in df_anx['rank']] #+"_"+str(r)
+	# record in DB
+	df_anx.to_sql('orderbooks',conn,if_exists='append')
+
+	
+	##POLONIEX 
+	#polo_markets = polo.load_markets()
+	#print(polo.id, polo_markets)
+	#print(polo.fetch_order_book("BTC/USDT"))
+	dictbook = polo.fetch_order_book("BTC/USDT")
+	a = pd.DataFrame(dictbook['asks'],columns=['askprice','askqty'])
+	a['rank']=a.index.values
+	b = pd.DataFrame(dictbook['bids'],columns=['bidprice','bidqty'])
+	b['rank']=b.index.values
+	df_polo = pd.merge(a,b,on='rank')
+	df_polo['source']='poloniex'
+	df_polo.index = [str(dictbook['timestamp']) for  r in df_polo['rank']] #+"_"+str(r)
+	# record in DB
+	df_polo.to_sql('orderbooks',conn,if_exists='append')
+	
+	##BitMex
+	#bmex_markets = bmex.load_markets()
+	#print(bmex.id, bmex_markets)
+	#print(bmex.fetch_order_book("BTC/USD"))
+	dictbook = bmex.fetch_order_book("BTC/USD")
+	a = pd.DataFrame(dictbook['asks'],columns=['askprice','askqty'])
+	a['rank']=a.index.values
+	b = pd.DataFrame(dictbook['bids'],columns=['bidprice','bidqty'])
+	b['rank']=b.index.values
+	df_bmex = pd.merge(a,b,on='rank')
+	df_bmex['source']='bitmex'
+	df_bmex.index = [str(dictbook['timestamp']) for  r in df_bmex['rank']] #+"_"+str(r)
+	# record in DB
+	df_bmex.to_sql('orderbooks',conn,if_exists='append')
+	
+	##BitFinex
+	#bfix_markets = bfix.load_markets()
+	#print(bfix.id, bfix_markets)
+	#print(bfix.fetch_order_book("BTC/USD"))
+	dictbook = bfix.fetch_order_book("BTC/USD")
+	a = pd.DataFrame(dictbook['asks'],columns=['askprice','askqty'])
+	a['rank']=a.index.values
+	b = pd.DataFrame(dictbook['bids'],columns=['bidprice','bidqty'])
+	b['rank']=b.index.values
+	df_bfix = pd.merge(a,b,on='rank')
+	df_bfix['source']='bitfinex'
+	df_bfix.index = [str(dictbook['timestamp']) for  r in df_bfix['rank']] #+"_"+str(r)
+	# record in DB
+	df_bfix.to_sql('orderbooks',conn,if_exists='append')
+	
+	"""
+	
+conn.close()
+
+'''
+#print(polo.fetch_ticker('BTC/USD'))
+#dfticker = pd.read_json(polo.fetch_ticker('BTC/USD'))
+
+#print(polo.fetch_trades('BTC/USD'))
+#print(exmo.fetch_balance())
+
+# sell one ฿ for market price and receive $ right now
+print(exmo.id, exmo.create_market_sell_order('BTC/USD', 1))
+
+# limit buy BTC/EUR, you pay €2500 and receive ฿1  when the order is closed
+print(exmo.id, exmo.create_limit_buy_order('BTC/EUR', 1, 2500.00))
+
+# pass/redefine custom exchange-specific order params: type, amount, price, flags, etc...
+kraken.create_market_buy_order('BTC/USD', 1, {'trading_agreement': 'agree'})
+
+'''
