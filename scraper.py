51f6adc1d19b93183be8da07a596e26efe5ec3f5
bluexm <Xavier.mathieu.office@gmail.com>
2017-12-12 18:55:39 +0800

    bug fix

--- scraper.py
+++ scraper.py
@@ -7,7 +7,8 @@ import csv
 import datetime 
 import pandas as pd 
 import Levenshtein as levs 
-import scraperwiki as ws 
+import scraperwiki as ws
+import pdb 
 
 ## user params 
 URL ='https://www.indeed.hk/jobs?q=Data+Scientist&start='
@@ -21,12 +22,13 @@ def dict_value(tuple):
 
 exectime = datetime.datetime.now()
 ht = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-
+# init csv 
 csvfile = open('scrape_indeed_'+ exectime.strftime("%Y%m%d")+'.csv', 'w', newline='',encoding="utf-8")
 writer = csv.writer(csvfile, delimiter=';', quotechar="'", quoting=csv.QUOTE_MINIMAL)
-
-excelDBfilename = 'scraping_indeed.xlsx'
-df = pd.read_excel(excelDBfilename, 'Sheet1', index_col=None, na_values=['NA'])
+# init dataframe 
+if RECORD_EXCEL:
+	excelDBfilename = 'scraping_indeed.xlsx'
+	df = pd.read_excel(excelDBfilename, 'Sheet1', index_col=None, na_values=['NA'])
 
 """ single ad page scrapers """
 def parse_workinginhongkong(pdata):
@@ -61,7 +63,6 @@ def parse_indeed(pdata):
 def parse_gobee(pdata):
 	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
 	adtitle = adtree.find("div", class_="job-header").find("h1").string
-	
 	if adtitle==None: 
 		return 'NA','NA','NA','NA','NA'
 	else: 
@@ -78,6 +79,23 @@ def parse_gobee(pdata):
 def parse_classywheeler(pdata):
 	return '','','','',''
 
+	
+## NOT FINISHED !!!
+def parse_whub(pdata):
+	pdb.set_trace()
+	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
+	root = adtree.find("h1", itemprop="title")
+	if root==None: 
+		return 'NA','NA','NA','NA','NA'
+	else:
+		addate = ''.join([s for s in adtree.find("p", itemprop="Created on :").stripped_strings])
+		adtitle = ''.join([s for s in adtree.find("div", class_="job-banner").findall("p")[2].stripped_strings])
+		adcompany = ''.join([s for s in adtree.find("span", itemprop="hiringOrganization").stripped_strings])
+		adjobdes = ''.join([s for s in adtree.find("p", class_="job-description").stripped_strings])
+		ademail = re.findall("[.\w]+@+\w+.com",pdata)
+		if ademail ==[]: ademail='NA'
+	return addate, adtitle, adjobdes, adcompany, ademail
+
 def parse_efinancialcareers(pdata):
 	adtree = bs4.BeautifulSoup(pdata, 'html.parser')
 	root = adtree.find("h1", itemprop="title")
@@ -92,8 +110,19 @@ def parse_efinancialcareers(pdata):
 		ademail = re.findall("[.\w]+@+\w+.com",pdata)
 		if ademail ==[]: ademail='NA'
 	return addate, adtitle, adjobdes, adcompany, ademail
-
+	
 """ scraping from indeed.hk """ 
+
+## declare parser functions to use according to site url (or word in the url)
+adparsers = {	"workinginhongkong": parse_workinginhongkong, 
+				"indeed":parse_indeed,
+				"gobee":parse_gobee,
+				"efinancialcareers":parse_efinancialcareers,
+				#"whub":parse_whub,
+				#"classywheeler": parse_classywheeler, 
+				#"efinancialcareers": parse_efinancialcareers
+			}
+
 res=[]
 for i in range(NBPAGESMAX):
 	try:
@@ -105,14 +134,7 @@ for i in range(NBPAGESMAX):
 		#print(tree.prettify())
 		content = tree.find_all("div",class_=re.compile("row"))
 		
-		## ad sites with parsers 
-		adparsers = {	"workinginhongkong": parse_workinginhongkong, 
-						"indeed":parse_indeed,
-						"gobee":parse_gobee,
-						"efinancialcareers":parse_efinancialcareers
-						#"classywheeler": parse_classywheeler, 
-						#"efinancialcareers": parse_efinancialcareers
-					}
+
 		## iterates on all search results 
 		for c in content:
 			rowres=[exectime.timestamp(), exectime.strftime("%Y-%m-%d %H:%M:%S")]
@@ -156,7 +178,8 @@ for i in range(NBPAGESMAX):
 				#res.append(rowres)
 			if dorecord:
 				# in dataframe for excel 
-				df = df.append( pd.DataFrame([rowres],columns=df.columns))
+				if RECORD_EXCEL:
+					df = df.append( pd.DataFrame([rowres],columns=df.columns))
 				# in SQLlite 
 				if RECORD_DB:
 					titles = ["epoch","scrping_dt","ad_cie_indeed","ad_jobtitle_indeed","search_ad_url","ad_url","ad_jobdate","ad_jobtitle","ad_jobcie","ad_jobdes","ad_email"] 
